8a32492ad499f297046b4ab883aa9e8eea2d7250
Dmitriy Emelyanov <emelyanov.dv@starline.ru>
2020-08-10 14:38:40 +0300

    Fix

--- docker/scripts/dev_talker.sh
+++ docker/scripts/dev_talker.sh
@@ -1,2 +1,7 @@
-docker run --network host --rm --name talker -it ros-docker-example \
-/bin/bash -c "source /catkin_ws/devel/setup.bash && roslaunch ros_docker_example ros_docker_example.launch"
+docker run \
+    --detach \
+    --network host \
+    --rm \
+    --name talker \
+    ros-docker-example \
+    /bin/bash -c "source /catkin_ws/devel/setup.bash && roslaunch ros_docker_example ros_docker_example.launch"
